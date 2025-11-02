f132071b7a98831c2fecd8b33c41eb6a53181e88
pratyushsawant <sawantpratyush05@gmail.com>
2025-11-02 09:27:18 -0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LandingPage } from "./landing-page/page";
+
+const Nitro = () => {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+};
+
+export default Nitro;
